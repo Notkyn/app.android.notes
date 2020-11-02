781646d1ca9077106f92b487f31dca420e28213e
Notkyn <notky@ukr.net>
2020-11-02 23:56:24 +0200

    step 5

--- app/src/main/java/ua/notky/notes/activity/MainActivity.java
+++ app/src/main/java/ua/notky/notes/activity/MainActivity.java
@@ -29,4 +29,10 @@ public class MainActivity extends AppCompatActivity{
             noteService.saveAll(getDefaulData());
         }
     }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+    }
 }
